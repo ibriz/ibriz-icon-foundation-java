8f4a0e2e28aea36d69c8238ca2e096af5edbbb64
sagarduwal <sagarduwal0102@hotmail.com>
2018-08-28 03:25:26 +0545

    First POC with JAVA ICON Foundation - change to key

--- grails-app/init/iconfoundationtest1/BootStrap.groovy
+++ grails-app/init/iconfoundationtest1/BootStrap.groovy
@@ -12,7 +12,7 @@ class BootStrap {
 
     def iconmainService
     def init = { servletContext ->
-        def walletMap = WalletOperations.loadWalletFromFile("C:\\Users\\morga\\Desktop\\Temp\\ICONFoundationTest1\\UTC--2018-08-24T13-35-41.671000000Z--hx65f6e18d378b57612a28f72acb97021eaa82aa5a.json")
+        def walletMap = WalletOperations.loadWalletFromFile("C:\\Users\\morga\\Desktop\\Temp\\ibriz-icon-foundation-java\\UTC--2018-08-24T13-35-41.671000000Z--hx65f6e18d378b57612a28f72acb97021eaa82aa5a.json")
         println "walletMap = $walletMap"
         String walletAddress = walletMap['address'] as String
         KeyWallet currentWallet = walletMap['wallet'] as KeyWallet
